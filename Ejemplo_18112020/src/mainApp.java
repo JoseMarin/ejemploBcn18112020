7623553d4e3ced5dfea111736c935b5c855d5220
Jose <jmarinmalagon@gmail.com>
2020-11-18 18:51:09 +0100

    Primer commit

---
+++ Ejemplo_18112020/src/mainApp.java
@@ -0,0 +1,18 @@
+
+public class mainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo!!!");
+		
+		int a = 10;
+		int b= 15;
+		int sum = a+b;
+		
+		System.out.println(sum);
+		
+		
+		
+	}
+
+}
